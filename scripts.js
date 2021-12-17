96a49519a449096390d5e08730385809dcfad88d
Juan Carlosl <juan2017amaral@gmail.com>
2021-12-17 20:58:02 -0300

    Current value

--- scripts.js
+++ scripts.js
@@ -1,17 +1,27 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.2
-const euro = 5.9
-const bitcoin = 276.131
+
+//Axios
 
 
-const convertvalue = () => {
+
+const convertvalue = async () => {
     const inputReal = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValue = document.getElementById('currency-value-text')
     //realValueText.innerHTML = inputReal
     //currencyValue.innerHTML = inputReal /dolar
+
+
+    //async await
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then( response => response.json())
+
+   
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
+
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: "BRL",
